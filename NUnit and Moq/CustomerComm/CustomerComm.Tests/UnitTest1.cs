afad085772ce48d41a14354dd0292c52c2b6c240
SubhashreeOjha <ojha.subhashree10@gmail.com>
2026-07-08 16:17:27 +0530

    Completed NUnit and Moq hands-on

---
+++ NUnit and Moq/CustomerComm/CustomerComm.Tests/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using Moq;
+using NUnit.Framework;
+using CustomerCommLib;
+
+namespace CustomerComm.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        private Mock<IMailSender> mockMailSender;
+        private CustomerCommLib.CustomerComm customerComm;
+
+        [SetUp]
+        public void Setup()
+        {
+            mockMailSender = new Mock<IMailSender>();
+            customerComm = new CustomerCommLib.CustomerComm(mockMailSender.Object);
+        }
+
+        [Test]
+        public void SendMailToCustomer_ShouldReturnTrue()
+        {
+            // Arrange
+            mockMailSender
+                .Setup(x => x.SendMail(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(true);
+
+            // Act
+            bool result = customerComm.SendMailToCustomer();
+
+            // Assert
+            Assert.That(result, Is.True);
+        }
+    }
+}
---
+++ NUnit and Moq/NUnitHandsOn/CalcLibrary/CalcLibrary.Tests/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using CalcLibrary;
+
+namespace CalcLibrary.Tests
+{
+    public class Tests
+    {
+        private MathLibrary math;
+
+        [SetUp]
+        public void Setup()
+        {
+            math = new MathLibrary();
+        }
+
+        [Test]
+        public void TestAdd()
+        {
+            Assert.That(math.Add(10, 20), Is.EqualTo(30));
+        }
+
+        [Test]
+        public void TestSub()
+        {
+            Assert.That(math.Sub(20, 10), Is.EqualTo(10));
+        }
+
+        [Test]
+        public void TestMul()
+        {
+            Assert.That(math.Mul(10, 5), Is.EqualTo(50));
+        }
+
+        [Test]
+        public void TestDiv()
+        {
+            Assert.That(math.Div(20, 5), Is.EqualTo(4));
+        }
+    }
+}
